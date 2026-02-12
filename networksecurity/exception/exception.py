c4dfe4ed6bb99157e736bffac0c96cea1f85c734
Ridhwan <ridhwanrazaliwork@gmail.com>
2026-02-12 21:32:19 +0800

    Add data ingestion stage

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -16,7 +16,5 @@ class NetworkSecurityException(Exception):
 if __name__=='__main__':
     try:
         logger.logging.info("Enter the try block")
-        a=1/0
-        print("This will not be printed",a)
     except Exception as e:
            raise NetworkSecurityException(e,sys)
