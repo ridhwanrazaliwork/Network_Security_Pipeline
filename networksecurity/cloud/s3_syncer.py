aefc0679ce316c4bde2514163c36bafeee927ccd
Ridhwan <ridhwanrazaliwork@gmail.com>
2026-02-14 22:10:11 +0800

    Add Docker, add S3 sync

--- networksecurity/cloud/s3_syncer.py
+++ networksecurity/cloud/s3_syncer.py
@@ -4,9 +4,9 @@ import os
 
 class S3Sync:
     def sync_folder_to_s3(self,folder,aws_bucket_url):
-        command = f"aws s3 sync {folder} {aws_bucket_url} "
+        command = f"aws s3 sync {folder} {aws_bucket_url} --region ap-southeast-1"
         os.system(command)
 
     def sync_folder_from_s3(self,folder,aws_bucket_url):
-        command = f"aws s3 sync  {aws_bucket_url} {folder} "
+        command = f"aws s3 sync  {aws_bucket_url} {folder} --region ap-southeast-1"
         os.system(command)
